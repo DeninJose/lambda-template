d04cb98baff7f6acb7e161d51ab6a40a09b66b4d
Denin Jose <deninjose8@gmail.com>
2025-05-14 10:40:15 +0530

    Testing

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -33,7 +33,7 @@ def lambda_handler(event, _):
     print("Received event:", event)
 
 
-    # Process the event (this is just a placeholder)
+    # Process the event
     response = {
         "statusCode": 200,
         "body": "Hello from Lambda!"
